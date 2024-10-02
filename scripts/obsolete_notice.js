110c25fd82ea8bfd10054bfaa274b22a0249e9e6
Mohd Fitri Zulkaffli <164287053+fitrizulkaffli@users.noreply.github.com>
2024-10-02 09:48:32 +0800

    Update obsolete_notice.js

--- scripts/obsolete_notice.js
+++ scripts/obsolete_notice.js
@@ -1,40 +1,40 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var popupOpen = false;
+    var popupOpen = false;
 
-  function openPopup() {
-    Swal.fire({
-      title: "This Website is Obsolete",
-      icon: "info",
-      iconColor: "#4169E1",
-      html: `This website is outdated and has been archived. It no longer reflects the current status or updates and is no longer affiliated with the company.`,
-      showCancelButton: true,
-      focusConfirm: false,
-      confirmButtonText: `I Understand`,
-      cancelButtonText: `Visit Facebook Page <i class="fa fa-external-link"></i>`,
-      footer: '<a href="https://fitri.my">This website was designed by Mohd Fitri Zulkaffli in 2017 <i class="fa fa-external-link"></i></a>',
-      confirmButtonColor: "#2A52BE",
-      cancelButtonColor: "#24252B",
-      allowOutsideClick: false,
-      allowEscapeKey: false,
-      allowEnterKey: false,
-    }).then((result) => {
-      if (result.isConfirmed) {
-        popupOpen = false;
-      } else if (result.dismiss === Swal.DismissReason.cancel) {
-        window.open("https://www.facebook.com/rinatechsdnbhd/", "noopener,noreferrer,nofollow");
-      }
+    function openPopup() {
+        Swal.fire({
+            title: "This Website is Obsolete",
+            icon: "info",
+            iconColor: "#FFC107",
+            html: `<br>This website is outdated and has been archived. It no longer reflects the current status or updates and is no longer affiliated with the company.`,
+            showCancelButton: true,
+            focusConfirm: false,
+            confirmButtonText: `I Understand`,
+            cancelButtonText: `Visit Facebook Page <i class="fa fa-external-link"></i>`,
+            footer: '<a href="https://fitri.my" class="custom-footer-link">This website was designed by Mohd Fitri Zulkaffli in 2017 <i class="fa fa-external-link"></i></a>',
+            confirmButtonColor: "#2A52BE",
+            cancelButtonColor: "#24252B",
+            allowOutsideClick: false,
+            allowEscapeKey: false,
+            allowEnterKey: false,
+        }).then((result) => {
+            if (result.isConfirmed) {
+                popupOpen = false;
+            } else if (result.dismiss === Swal.DismissReason.cancel) {
+                window.open("https://www.facebook.com/rinatechsdnbhd/", "noopener,noreferrer,nofollow");
+            }
+        });
+        popupOpen = true;
+    }
+    window.addEventListener('blur', function() {
+        if (popupOpen) {
+            Swal.close();
+        }
     });
-    popupOpen = true;
-  }
-  window.addEventListener('blur', function() {
-    if (popupOpen) {
-      Swal.close();
-    }
-  });
-  window.addEventListener('focus', function() {
-    if (popupOpen) {
-      openPopup();
-    }
-  });
-  openPopup();
+    window.addEventListener('focus', function() {
+        if (popupOpen) {
+            openPopup();
+        }
+    });
+    openPopup();
 });
